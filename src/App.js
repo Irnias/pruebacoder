f85b6ab1917ee82f89b8e2e25b648381eaf149e2
Rama D'Accorso <ramiro.daccorso@gmail.com>
2022-03-21 20:12:07 -0300

    Modificando el archivo app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Proyecto prueba Rama
         </a>
       </header>
     </div>
